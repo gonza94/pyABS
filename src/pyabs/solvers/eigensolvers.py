da25e9fe74daa9184099ecaf3d397efded1307d8
Jacob Dulya <jdulya03@gmail.com>
2026-08-19 17:35:57 -0500

    added initial eigensystem solutions and another example file

--- src/pyabs/solvers/eigensolvers.py
+++ src/pyabs/solvers/eigensolvers.py
@@ -23,8 +23,32 @@ class Solution:
     eigenvalues: np.ndarray
     eigenvectors: np.ndarray | None = None
     antheta: np.ndarray | None = None
+    alpha: np.ndarray | None  = None
     azimuthal: np.ndarray | None = None
-    total_solution : np.ndarray | None = None 
+
+    def compute_x(self, theta: float, psi: float):
+        an = self.antheta(theta)
+        az = self.azimuthal(psi)
+        soln = 0
+        for n, coeff in enumerate(self.eigenvalues):
+            term = an[n]*az[n]
+            soln = soln + term
+        return soln
+
+    def compute_xplus(self, theta: float, s: float):
+        #an = self.antheta(theta)
+        #az = self.azimuthal(psi)
+        return self.compute_x(theta=theta, psi=(-np.pi*s))
+
+    def compute_xminus(self, theta: float, s: float):
+        return self.compute_x(theta=theta, psi=(np.pi*s))
+
+    def compute_centroid(self, psi: float, *, theta: float = 0.0):
+        soln = 0
+        for n, coeff in enumerate(self.eigenvalues):
+            term = (self.compute_x(theta, psi) + self.compute_x(theta, -psi))/2
+            soln = soln + term
+        return soln
 
 @dataclass
 class EigenSolution:
@@ -73,9 +97,42 @@ def solve_eigensystem(matrix: Array, *, vectors: bool=True) -> EigenSolution:
     return EigenSolution(eigenvalues=eigenvalues,
                          eigenvectors=None,)
 
-def solve_antheta(eigsys: EigenSolution):
-    antheta = lambda theta : np.sum(eigsys.eigenvectors*np.exp((eigsys.eigenvalues)*theta), axis=1)
-    return Solution(eigenvalues=eigsys.eigenvalues, eigenvectors=eigsys.eigenvectors, antheta=antheta)
+def solve_system(matrix: Array, *, A0: complex = 1.0) -> Solution:
+    eigsys = solve_eigensystem(matrix)
+    alpha = solve_alpha(eigsys, A0=A0)
+    antheta = solve_antheta(eigsys, alpha=alpha)
+    azimuth=solve_azimuthal(eigsys)
+    return Solution(eigenvalues=eigsys.eigenvalues, eigenvectors=eigsys.eigenvectors, 
+                    antheta=antheta, alpha=alpha, azimuthal=azimuth)
+
+
+#def solve_antheta(eigsys: EigenSolution):
+#    antheta = lambda theta : np.sum(eigsys.eigenvectors*np.exp((eigsys.eigenvalues)*theta), axis=1)
+#    return antheta
+
+def solve_alpha(eigsys: EigenSolution, *, A0: complex = 1.0):
+    size = int((int(eigsys.eigenvalues.shape[0])-1)/2)
+    target = np.zeros(eigsys.eigenvalues.shape[0])
+    target[size] = A0
+    alpha = np.linalg.solve(eigsys.eigenvectors, target)
+    return alpha
+
+def solve_antheta(eigsys: EigenSolution, alpha: np.npdarray):
+    term = lambda theta: eigsys.eigenvectors*np.exp((eigsys.eigenvalues)*theta)
+    antheta = lambda theta: np.sum(alpha*term(theta), axis=1)
+    return antheta
+
+def solve_azimuthal(eigsys: EigenSolution):
+    n_max = (int(eigsys.eigenvalues.shape[0]) - 1)/2
+    nvec= np.arange(-n_max, n_max+1, dtype=int)
+    azimuth = lambda psi: np.exp((1j)*nvec*psi)
+    return azimuth
+
+def scan_over_w(basis: Any, *, w: Array) -> EigenScanResult:
+    print("I don't do anything yet")
+    return EigenScanResult(sc = sc, eigenvalues=eigenvalues)
+
+
 
 def scan_eigenvalues_over_sc(basis: Any, *, q: Array) -> EigenScanResult:
     sc = np.asarray(sc, dtype=complex)
